8ac1c32bb34aa088861b905de64b35025da57762
yashrajpchavda <yashrajpchavda@gmail.com>
2016-07-19 09:23:14 +0530

    Implemented interpolation.

--- ScopeAndInterpolation/app.js
+++ ScopeAndInterpolation/app.js
@@ -1,1 +1,13 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller( 'mainController', [ '$scope', '$timeout', function( $scope, $timeout ) {
+
+    $scope.name = 'Jimmy';
+
+    $timeout( function() {
+
+        $scope.name = 'Everybody';
+
+    }, 3000 );
+
+} ] );
