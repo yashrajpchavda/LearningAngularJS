1415928c50e138a0fc15fedb0dfcaeac134b29d9
yashrajpchavda <yashrajpchavda@gmail.com>
2016-07-19 22:17:06 +0530

    Added an example for the watcher and the digest cycle.

--- Watchers-and-digest-cycle/app.js
+++ Watchers-and-digest-cycle/app.js
@@ -1,6 +1,6 @@
 var myApp = angular.module( 'myApp', [] );
 
-myApp.controller( 'mainController', [ '$scope', '$filter', function( $scope, $filter ) {
+myApp.controller( 'mainController', [ '$scope', '$filter', '$log', function( $scope, $filter, $log ) {
     "use strict";
 
     $scope.myHandle = '';
@@ -8,5 +8,25 @@ myApp.controller( 'mainController', [ '$scope', '$filter', function( $scope, $fi
     $scope.lowercaseHandle = function() {
         return $filter( 'lowercase' )( $scope.myHandle );
     };
+
+    $scope.$watch( 'myHandle', function( newValue, oldValue ) {
+
+        $log.info( 'changed' );
+        $log.log( 'old: ' + oldValue );
+        $log.log( 'new: ' + newValue );
+
+    } );
+
+    setTimeout( function() {
+
+        $scope.$apply( function() {
+
+            $scope.myHandle = 'somethingnew';
+            console.log( 'scope changed !' );
+
+        } );
+
+
+    }, 3000 );
 
 } ] );
