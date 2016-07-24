7d63ae90d954ed1f052dbe56586205aa9f90ebd9
yashrajpchavda <yashrajpchavda@gmail.com>
2016-07-24 11:37:21 +0530

    Added routes and controllers.

--- Weather-forecast/app.js
+++ Weather-forecast/app.js
@@ -1,1 +1,44 @@
-var myApp = angular.module( 'myApp', [ 'ngRoute' ] );
+// MODULE
+var weatherApp = angular.module( 'weatherApp', [ 'ngRoute', 'ngResource' ] );
+
+// ROUTE CONFIG
+weatherApp.config( [ '$routeProvider', function( $routeProvider ) {
+    'use strict';
+
+    $routeProvider
+
+    .when( '/', {
+
+        templateUrl: 'pages/home.html',
+        controller: 'homeController'
+
+    } )
+
+    .when( '/forecast', {
+
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+
+    } )
+
+    .when( '/forecast/:days', {
+
+        templateUrl: 'pages/forecast.html',
+        controller: 'forecastController'
+
+    } );
+
+} ] );
+
+// CONTROLLERS
+weatherApp.controller( 'homeController', [ '$scope', function( $scope ) {
+    'use strict';
+
+} ] );
+
+weatherApp.controller( 'forecastController', [ '$scope', '$routeParams', function( $scope, $routeParams ) {
+    'use strict';
+
+    $scope.days = $routeParams.days || 1;
+
+} ] );
